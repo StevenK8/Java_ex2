1c641ce9d08e9857dfb3776e599cc96ffa30b2db
STEVEN KERAUTRET <steven928@hotmail.fr>
2019-10-10 11:48:03 +0200

    2.4.2 Add swaptab

--- tableau/src/main/java/tableau/App.java
+++ tableau/src/main/java/tableau/App.java
@@ -6,15 +6,25 @@ public final class App {
 
     public static void main(String[] args) {
         String[] tab = {"1","2","123","ok","23"};
+        String[] tab2 = {"24","11","1303","89","-78"};
         int[] intTab = convertTab(tab);
+        int[] intTab2 = convertTab(tab2);
         //showTab(intTab);
-        System.out.println(maxVal(intTab));
+        //System.out.println(maxVal(intTab));
+        int [][] swappedTabs = swapMinMax(intTab, intTab2);
+        showTab(swappedTabs[0]);
+        showTab(swappedTabs[1]);
     }
 
     public static void showTab(int[] intTab){
+        System.out.print("[");
         for(int i=0; i<intTab.length; i++){
-            System.out.println(intTab[i]);
+            if(i!=intTab.length-1)
+                System.out.print(intTab[i] + ",");
+            else
+                System.out.print(intTab[i]);   
         }
+        System.out.print("]\n");
     }
 
     public static int maxVal(int[] tab){
@@ -29,6 +39,31 @@ public final class App {
         }else
             System.out.println("Tab is empty");
             return 1;
+    }
+
+    public static int[][] swapMinMax(int[] min, int[] max){
+        int valMin = min[0];
+        int posMin = 0;
+        int valMax = max[0];
+        int posMax = 0;
+        for(int i=0; i<min.length; i++){
+            if(valMin>min[i]){
+                valMin = min[i];
+                posMin = i;
+            }
+        }
+        for(int i=0; i<max.length; i++){
+            if(valMax<max[i]){
+                valMax = max[i];
+                posMax = i;
+            }
+        }
+        int temp = min[posMin];
+        min[posMin] = max[posMax];
+        max[posMax] = temp;
+
+        int[][] outputTab = {min,max};
+        return outputTab;
     }
 
     public static int[] convertTab(String[] tab) throws NumberFormatException{
